70e34019c8cc52e4838e9a20707bdf48504cf0d9
jiwen xin <kevinxin@scripps.edu>
2019-12-02 10:34:06 -0800

    add travis test

---
+++ config.js
@@ -0,0 +1,12 @@
+exports.SEMANTIC2API = {
+    "Gene": "mygene.info",
+    "SequenceVariant": "myvariant.info",
+    "ChemicalSubstance": ["mychem.info", "umlschem"],
+    "DiseaseOrPhenotypicFeature": "mydisease.info",
+    "AnatomicalEntity": "semmedanatomy",
+    "PhenotypicFeature": "semmedphenotype",
+    "Pathway": "pathway",
+    "MolecularActivity": "mf",
+    "CellularComponent": "cc",
+    "BiologicalProcess": "bp"
+}
