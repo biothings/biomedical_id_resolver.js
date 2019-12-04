cb20c7b0b2f5d9717e44768d471a0d9ee59a1e3b
jiwen xin <kevinxin@scripps.edu>
2019-12-04 10:29:21 -0800

    add construct api call function

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const _ = require('lodash');
 const APIMETA = require('./config').APIMETA;
+const axios = require('axios').default;
 
 exports.findAPIByType = function(semanticType, idType) {
     if (typeof semanticType !== "string" || typeof idType !== "string") {
@@ -10,3 +11,42 @@ exports.findAPIByType = function(semanticType, idType) {
             && (_.keys(o['field_mapping']).includes(idType.toLowerCase()))
     })
 }
+
+/**
+ * construct a BioThings batch query using axios
+ * The query aims to fetch all equivalent IDs for the inputs
+ * The return value is an array of axios post query promises
+ * @param {array} inputs - Input IDs
+ * @param {string} prefix - The ID type of the inputs, e.g. hgnc
+ * @param {string} api - The API used to query the input IDs 
+ * @returns - The array of axios post query promises
+ */
+exports.constructPostQuery = function(inputs, prefix, api) {
+    let query = 'q={inputs}&scopes={scopes}&fields={fields}&dotfield=true';
+    inputs = inputs.join(',');
+    if (!(api in APIMETA)) {
+        return undefined;
+    }
+    let meta = APIMETA[api];
+    let fields = _.values(meta['field_mapping']).join(',');
+    if (!(prefix in meta['field_mapping'])){
+        return undefined;
+    }
+    let scopes = meta['field_mapping'][prefix];
+    return axios({
+        method: 'post',
+        url: meta['base_url'],
+        data: query.replace('{inputs}', inputs).replace('{scopes}', scopes).replace('{fields}', fields),
+        headers: {'content-type': 'application/x-www-form-urlencoded'}
+    })
+}
+
+// construct post query
+// result should be an array, input ids should be chunked by 1000
+
+
+// make API call
+
+// transform API response into BioLink model
+
+// 
--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 const expect = require("chai").expect;
 const findAPIByType = require('../index').findAPIByType;
+const apiCall = require('../index').constructPostQuery;
 
 describe("Test findAPIByType functions", function() {
     it("if semantic type is not a string, return undefined", function() {
@@ -21,5 +22,28 @@ describe("Test findAPIByType functions", function() {
         expect(findAPIByType(semantic_type, id_type)).undefined;
         id_type = '';
         expect(findAPIByType(semantic_type, id_type)).undefined;
+    });
+})
+
+describe("Test constructPostQuery function", function() {
+    it("if api is not in config, return undefined", function() {
+        let api = 'mykkk.com';
+        expect(apiCall(['1017', '1018'], 'hgnc', api)).undefined;
+    });
+    it("if prefix is not in api's fields, return undefined", function() {
+        let api = 'mygene.info';
+        let prefix = 'uuu';
+        expect(apiCall(['1017', '1018'], prefix, api)).undefined;
+    });
+    it("return a axios promise for mygene.info", async function() {
+        let api = 'mygene.info';
+        let prefix = 'entrez';
+        let ids = ['1017', '1018']
+        let res = await apiCall(ids, prefix, api);
+        expect(res.data).to.be.an.instanceof(Array).to.have.lengthOf(2);
+        let res1 = res.data[0];
+        expect(res1).to.include({'entrezgene': '1017', 'MIM': "116953", 'ensembl.gene': 'ENSG00000123374', 'HGNC': '1771', 'umls.cui': 'C1332733', 'symbol': 'CDK2', 'name': 'cyclin dependent kinase 2'});
+        let res2 = res.data[1];
+        expect(res2).to.include({'entrezgene': '1018', 'symbol': 'CDK3', 'name': 'cyclin dependent kinase 3'});
     });
 })
