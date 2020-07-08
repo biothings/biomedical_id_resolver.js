b82faa9471451128d1b187c5a4668a06d721945c
jiwen xin <kevinxin@scripps.edu>
2020-07-08 10:48:17 -0700

    add test

---
+++ test/index.test.js
@@ -0,0 +1,56 @@
+const resolve = require("../src/index");
+
+describe("Test resolve functions", () => {
+    test("Test using NCBIGene IDs as input", async () => {
+        const input = {
+            Gene: ["NCBIGene:1017", "NCBIGene:1018"]
+        };
+        let res = await resolve(input);
+        expect(Object.keys(res)).toContain("NCBIGene:1017");
+        expect(Object.keys(res["NCBIGene:1017"])).toContain("id");
+        expect(Object.keys(res["NCBIGene:1017"])).toContain("ids");
+        expect(Object.keys(res["NCBIGene:1017"])).toContain("bte_ids");
+        expect(Object.keys(res["NCBIGene:1017"])).toContain("equivalent_identifiers");
+        expect(Object.keys(res["NCBIGene:1017"])).toContain("type");
+        expect(Object.keys(res["NCBIGene:1017"]).length).toBe(5);
+        expect(Object.keys(res).length).toBe(2);
+    });
+    test("Test using mixed Gene ID as input", async () => {
+        const input = {
+            Gene: ["NCBIGene:1017", "HGNC:1177"]
+        };
+        let res = await resolve(input);
+        expect(Object.keys(res)).toContain("HGNC:1177");
+    });
+    test("Test using UBERON IDs", async () => {
+        const input = {
+            AnatomicalEntity: ["UBERON:0007173", "UBERON:0006849"]
+        };
+        let res = await resolve(input);
+        expect(Object.keys(res)).toContain("UBERON:0007173");
+        expect(res["UBERON:0007173"].type).toBe("AnatomicalEntity");
+    });
+    test("Test IDs from mixed semantic types", async () => {
+        const input = {
+            Gene: ["NCBIGene:1017"],
+            AnatomicalEntity: ["UBERON:0007173", "UBERON:0006849"]
+        };
+        let res = await resolve(input);
+        expect(Object.keys(res).length).toBe(3);
+        expect(Object.keys(res)).toContain("UBERON:0007173");
+        expect(res["UBERON:0007173"].type).toBe("AnatomicalEntity");
+        expect(Object.keys(res)).toContain("NCBIGene:1017");
+        expect(res["NCBIGene:1017"].type).toBe("Gene");
+    });
+    test("Test invalid iids", async () => {
+        const input = {
+            Gene: ["NCBIGene:1017", "cc:133"],
+            AnatomicalEntity: ["UBERON:0007173", "UBERON:0006849"]
+        };
+        let res = await resolve(input);
+        expect(Object.keys(res).length).toBe(4);
+        expect(Object.keys(res)).toContain("cc:133");
+        expect(res["cc:133"].type).toBe("Gene");
+        expect(res["cc:133"].flag).toBe("failed");
+    });
+})
