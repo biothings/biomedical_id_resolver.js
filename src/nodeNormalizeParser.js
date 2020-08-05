b2e6d5ebf1c0bdfd718707426d8d4a782cf62a5b
jiwen xin <kevinxin@scripps.edu>
2020-08-05 16:54:45 -0700

    update output structure

--- src/nodeNormalizeParser.js
+++ src/nodeNormalizeParser.js
@@ -1,5 +1,6 @@
 const camelcase = require("camelcase");
 const config = require("../config");
+const utils = require("./utils");
 
 module.exports = class NodeNormalizeParser {
     constructor(response, mapping) {
@@ -8,43 +9,32 @@ module.exports = class NodeNormalizeParser {
     }
 
     parse() {
+        let res = {};
         Object.keys(this.response).map(curie => {
             if (this.response[curie] === null) {
-                this.response[curie] = {
-                    id: {
-                        identifier: curie,
-                        label: curie
-                    },
-                    equivalent_identifiers: [
-                        {
-                            identifier: curie
-                        }
-                    ],
-                    bte_ids: {
-                        [curie.split(':')[0]]: curie
-                    },
-                    ids: [curie],
-                    type: this.mapping[curie],
-                    flag: "failed"
-                };
+                res[curie] = utils.generateFailedResponse(curie, this.mapping[curie]);
             } else {
-                this.response[curie].type = camelcase(this.response[curie].type[0], { pascalCase: true });
-                this.response[curie].bte_ids = {};
-                this.response[curie].ids = [];
+                let tmp = {
+                    primary: this.response[curie].id,
+                    curies: [],
+                    db_ids: {},
+                    type: camelcase(this.response[curie].type[0], { pascalCase: true })
+                };
                 this.response[curie].equivalent_identifiers.map(item => {
-                    this.response[curie].ids.push(item.identifier);
+                    tmp[curie].curies.push(item.identifier);
                     let [prefix, value] = item.identifier.split(':');
-                    if (!(prefix in this.response[curie].bte_ids)) {
-                        this.response[curie].bte_ids[prefix] = [];
+                    if (!(prefix in tmp[curie].db_ids)) {
+                        tmp[curie].db_ids[prefix] = [];
                     }
                     if (config.CURIE.ALWAYS_PREFIXED.includes(prefix)) {
-                        this.response[curie].bte_ids[prefix].push(item.identifier);
+                        tmp[curie].db_ids[prefix].push(item.identifier);
                     } else {
-                        this.response[curie].bte_ids[prefix].push(value);
+                        tmp[curie].db_ids[prefix].push(value);
                     }
-                })
+                });
+                res[curie] = tmp;
             }
         });
-        return this.response;
+        return res;
     }
 }
