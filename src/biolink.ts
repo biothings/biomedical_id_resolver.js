fcc128151e6902265abbb346ef7b0d2bce7bc4ad
jiwen xin <kevinxin@scripps.edu>
2021-02-26 11:26:09 -0800

    refactor: :recycle: add biolink handler module

---
+++ src/biolink.ts
@@ -0,0 +1,25 @@
+import { BioLink } from 'biolink-model';
+import { BioLinkHandlerClass } from './common/types'
+
+class BioLinkHandler implements BioLinkHandlerClass {
+    private static instance: BioLinkHandler;
+    private _classTree: BioLink["classTree"];
+
+    constructor() {
+        if (!BioLinkHandler.instance) {
+            const biolink = new BioLink();
+            biolink.loadSync();
+            this._classTree = biolink.classTree;
+        }
+        return BioLinkHandler.instance;
+    }
+
+    get classTree() {
+        return this._classTree;
+    }
+}
+
+const BioLinkHandlerInstance = new BioLinkHandler();
+Object.freeze(BioLinkHandlerInstance);
+
+export default BioLinkHandlerInstance;
