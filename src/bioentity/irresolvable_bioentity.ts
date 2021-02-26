7e2941cf43b7129a5df659ed4d4d082d01542bb7
jiwen xin <kevinxin@scripps.edu>
2021-02-26 11:27:15 -0800

    refactor: :recycle: add irresolvable bioentity class

---
+++ src/bioentity/irresolvable_bioentity.ts
@@ -0,0 +1,35 @@
+import { DBIdsObject } from '../common/types';
+import { getPrefixFromCurie, generateDBID } from '../utils';
+import { BioEntity } from './base_bioentity';
+
+export class IrresolvableBioEntity extends BioEntity {
+  private _semanticType: string;
+  private curie: string;
+  constructor(semanticType: string, curie: string) {
+    super();
+    this._semanticType = semanticType;
+    this.curie = curie;
+  }
+
+  get semanticType(): string {
+    return this._semanticType;
+  }
+
+  get primaryID(): string {
+    return this.curie;
+  }
+
+  get label(): string {
+    return this.curie;
+  }
+
+  get curies(): string[] {
+    return [this.curie];
+  }
+
+  get dbIDs(): DBIdsObject {
+    return {
+      [getPrefixFromCurie(this.curie)]: [this.curie],
+    };
+  }
+}
