14739955541432ad6cbc89f69b8378e5c5fb57fb
jiwen xin <kevinxin@scripps.edu>
2021-02-27 10:45:53 -0800

    refactor: :recycle: create an async query function

---
+++ src/query/index.ts
@@ -0,0 +1,24 @@
+import { Scheduler } from './scheduler';
+import { DBIdsObject, IndividualResolverOutput } from '../common/types';
+
+
+import Debug from 'debug';
+const debug = Debug('biomedical-id-resolver:Query');
+
+const query = async (resolvable: DBIdsObject): Promise<IndividualResolverOutput[]> => {
+    const scheduler = new Scheduler(resolvable);
+    scheduler.schedule();
+    let result = [];
+    for (const promises of Object.values(scheduler.buckets)) {
+        const res = (await Promise.allSettled(promises)) as any;
+        res.map((item) => {
+            if (item.status === 'fulfilled') {
+                result.push(item.value);
+            } else {
+                debug(`One API Query fails, reason is ${item.reason}`);
+            }
+        });
+    }
+    return result;
+}
+export default query;
