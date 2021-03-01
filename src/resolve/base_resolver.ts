26dfcea1ca6d1186dcc7de548ecc514161531932
jiwen xin <kevinxin@scripps.edu>
2021-03-01 10:44:41 -0800

    refactor: :recycle: add base resolver abstract class

--- src/resolve/base_resolver.ts
+++ src/resolve/base_resolver.ts
@@ -1,5 +1,21 @@
-import { IResolver, ResolverOutput } from '../common/types';
+import { IResolver, ResolverOutput, DBIdsObject } from '../common/types';
+import { IrresolvableBioEntity } from '../bioentity/irresolvable_bioentity';
+import Debug from 'debug';
+const debug = Debug('biomedical-id-resolver:resolver');
 
 export default abstract class BaseResolver implements IResolver {
+    protected annotateUnresolvedInputs(unresolvedInputs: DBIdsObject, currentResult: ResolverOutput): ResolverOutput {
+        Object.keys(unresolvedInputs).map((semanticType) => {
+            for (const curie of unresolvedInputs[semanticType]) {
+                if (!(curie in currentResult)) {
+                    currentResult[curie] = [];
+                }
+                currentResult[curie].push(new IrresolvableBioEntity(semanticType, curie));
+            }
+        });
+        debug(`Total number of Irresolvable curies are: ${Object.keys(unresolvedInputs).length}`);
+        return currentResult;
+    }
+
     abstract resolve(usrInput: unknown): Promise<ResolverOutput>
 }
