27ce8fdd8f4fd91d93612d7180e60524665c697d
Marco Cano <artofmarco@gmail.com>
2021-10-12 13:54:30 -0700

    fix: :sparkles: rename chemblmax phase field to FDA approved

--- src/config.ts
+++ src/config.ts
@@ -136,7 +136,7 @@ export const APIMETA: MetaDataItemsObject = {
       name: ['chembl.pref_name', 'drugbank.name', 'umls.name', 'ginas.preferred_name', 'pharmgkb.name', 'chebi.name'],
     },
     additional_attributes_mapping: {
-      chembl_max_phase: ['chembl.max_phase'],
+      'biolink:highest_FDA_approval_status': ['chembl.max_phase'],
       chembl_molecule_type: ['chembl.molecule_type'],
       drugbank_drug_category: ['drugbank.categories.category'],
       drugbank_taxonomy_class: ['drugbank.taxonomy.class'],
@@ -205,7 +205,7 @@ export const APIMETA: MetaDataItemsObject = {
       name: ['chembl.pref_name', 'drugbank.name', 'umls.name', 'ginas.preferred_name', 'pharmgkb.name', 'chebi.name'],
     },
     additional_attributes_mapping: {
-      chembl_max_phase: ['chembl.max_phase'],
+      'biolink:highest_FDA_approval_status': ['chembl.max_phase'],
       chembl_molecule_type: ['chembl.molecule_type'],
       drugbank_drug_category: ['drugbank.categories.category'],
       drugbank_taxonomy_class: ['drugbank.taxonomy.class'],
