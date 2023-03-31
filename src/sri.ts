b8122f1b534ec4add7ee26fda863828ff79696f8
Jackson Callaghan <43009413+tokebe@users.noreply.github.com>
2023-03-31 16:05:08 -0400

    fix: lower NodeNorm batch size 5000->1000

--- src/sri.ts
+++ src/sri.ts
@@ -21,7 +21,7 @@ function combineInputs(userInput: ResolverInput): string[] {
 async function query(api_input: string[]) {
   let url = 'https://nodenorm.transltr.io/get_normalized_nodes';
 
-  let chunked_input = _.chunk(api_input, 5000);
+  let chunked_input = _.chunk(api_input, 1000);
   try {
     const userAgent = `BTE/${process.env.NODE_ENV === 'production' ? 'prod' : 'dev'} Node/${process.version} ${
       process.platform
